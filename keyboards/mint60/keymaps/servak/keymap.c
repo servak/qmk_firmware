5200c4eaba517ae74d90dc5dc7b0cd4f937eadfe
servak <fservak@gmail.com>
2018-10-09 00:35:30 +0900

    update mode

--- keyboards/mint60/keymaps/servak/keymap.c
+++ keyboards/mint60/keymaps/servak/keymap.c
@@ -36,9 +36,30 @@ enum custom_keycodes {
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  /* Qwerty
+  /* Default
    * ,-----------------------------------------.      ,-----------------------------------------------------.
    * |   `  |   1  |   2  |   3  |   4  |   5  |      |   6  |   7  |   8  |   9  |   0  |  -   |  =  | Del |
+   * |------+------+------+------+------+------|      |------+------+------+------+------+------|-----------|
+   * | Tab  |   Q  |   W  |   E  |   R  |   T  |      |   Y  |   U  |   I  |   O  |   P  | Bksp |  [  | \   |
+   * |------+------+------+------+------+------|      |------+------+------+------+------+------|-----------|
+   * | Ctrl |   A  |   S  |   D  |   F  |   G  |      |   H  |   J  |   K  |   L  |   ;  |  '   |   Enter   |
+   * |------+------+------+------+------+------|      |------+------+------+------+------+------|-----------|
+   * | Shift|   Z  |   X  |   C  |   V  |   B  |      |   N  |   M  |   ,  |   .  |   /  |Shift | Up  |Mode |
+   * |------+------+------+------+------+------|      |------+------+------+------+------+------|-----|-----|
+   * | Mode | Esc  |  Alt  | GUI |       Space |      |R_ALT | Esc | R_Alt | Left | Down |  Right |
+   * `-----------------------------------------|      |-----------------------------------------'
+   */
+  [0] = LAYOUT( \
+      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL, KC_BSPC,  \
+      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  \
+      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,   \
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,   KC_UP,  MO(1),   \
+      MO(1),   KC_ESC,  KC_LALT, KC_LGUI, KC_SPC,             KC_RALT, KC_ESC,  KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT \
+      ),
+
+  /* Mode 1
+   * ,-----------------------------------------.      ,-----------------------------------------------------.
+   * |  ESC  |  F1  |  F2  |  F3  |  F4  |  F5  |      |  F6  |  F7  |  F8  |  F9  | F10  | F11  | F12 | Del|
    * |------+------+------+------+------+------|      |------+------+------+------+------+------|-----------|
    * | Tab  |   Q  |   W  |   E  |   R  |   T  |      |   Y  |   U  |   I  |   O  |   P  | Bksp |  [  | \   |
    * |------+------+------+------+------+------|      |------+------+------+------+------+------|-----------|
@@ -49,14 +70,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |Adjust| Esc  |  Alt  | GUI |       Space |      |R_ALT | Esc | R_Alt | Left | Down |  Right |
    * `-----------------------------------------|      |-----------------------------------------'
    */
-  [0] = LAYOUT( \
-      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL, KC_BSPC,  \
+  [1] = LAYOUT( \
+      KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,    KC_DEL, \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  \
-      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,   \
+      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_SCLN, KC_QUOT, KC_ENT,   \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,   KC_UP,  MO(1),   \
       KC_ESC,  KC_ESC,  KC_LALT, KC_LGUI, KC_SPC,             KC_RALT, KC_ESC,  KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT \
-      ),
-  [1] = LAYOUT( \
+  ),
+
+  [2] = LAYOUT( \
     KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,    KC_DEL, \
     RGB_TOG,   RGBRST,  RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
     XXXXXXX,     RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,        XXXXXXX, \
